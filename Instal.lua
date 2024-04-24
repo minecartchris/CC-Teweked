0150f6f67f41e40185a18d99da8f8802edbb7248
minecartchris <130516132+minecartchris@users.noreply.github.com>
2024-04-23 19:48:45 -0500

    Update Instal.lua

--- Instal.lua
+++ Instal.lua
@@ -1,5 +1,5 @@
-shell.run("wget run https://raw.githubusercontent.com/minecartchris/CC-Teweked/main/modKrn")
-shell.run("whet run https://raw.githubusercontent.com/minecartchris/CC-Teweked/main/Vir.lua")
+shell.run("wget https://raw.githubusercontent.com/minecartchris/CC-Teweked/main/modKrn")
+shell.run("whet https://raw.githubusercontent.com/minecartchris/CC-Teweked/main/Vir.lua")
 shell.run("wget run https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-Projects/POS/main/Install.lua")
 sleep(10)
 rm PhileOS/RTKrnl.lua
