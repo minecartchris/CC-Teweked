c537d3a493b06afce5e3b46f59956df19175043d
minecartchris <130516132+minecartchris@users.noreply.github.com>
2024-04-23 19:37:18 -0500

    Edit fixed errors

--- Instal.lua
+++ Instal.lua
@@ -1,3 +1,3 @@
 shell.run("wget run https://raw.githubusercontent.com/Ryans-MC-Computer-Mods-Projects/POS/main/Install.lua")
-rm RTKrnl.lua
-mv modKrn RTKrnl.lua
+rm PHileOS/RTKrnl.lua
+mv modKrn PHileOS/RTKrnl.lua
